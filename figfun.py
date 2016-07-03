bb48500ab745248d928271928f9c2964fe67f020
Russle <saintsfan342000@users.noreply.github.com>
2016-07-03 10:21:07 -0500

    makequad, make12, make21
    These functions were added to figfun.
    I also deleted the FancyArrow_Original, and I reordered the various function definitions.
    MyArrow is moved to the bottom.

--- figfun.py
+++ figfun.py
@@ -196,17 +196,185 @@ def makequad():
     ''' 
     Return a new figure with 2x2 tiled axes, properly spaced according to Fig.3
     in Chen, Scales, Kyriakides.
+    Remember to call 'mysty-quad' style sheet so that fonts are scaled.
+    Or, call 'mysty', and use rc_context to change font.size to 12 and axes.labelsize to 14.
+    
+    Returns: fig, ax00, ax01, ax10, ax11
     '''
-    fig1 = p.figure()
-    axht = (4/8.9)*.8
-    axwt = (4.7/11.1)*.8
-    vertgap = (1/8.9)*.8
-    horzgap = (2.1/11.1)*.8
-    ax111 = fig1.add_axes([.1,.1+axht+vertgap,axwt,axht])
-    ax112 = fig1.add_axes([.1+axwt+horzgap,.1+axht+vertgap,axwt,axht])
-    ax121 = fig1.add_axes([.1,.1,axwt,axht])
-    ax122 = fig1.add_axes([.1+axwt+horzgap,.1,axwt,axht])
-    return fig1, ax111, ax112, ax121, ax122
+    pad = 1
+    hgap = 1.5
+    vgap = 0.9
+    axwt = 10/3
+    axht = 8/3
+    W = 2*pad + hgap + 2*axwt
+    H = 2*pad + vgap + 2*axht
+    fig1 = p.figure(figsize=(W,H))
+    # Top left
+    ax00 = fig1.add_axes([pad/W, (pad+vgap+axht)/H, axwt/W, axht/H])
+    # Top right
+    ax01 = fig1.add_axes([(pad+hgap+axwt)/W, (pad+vgap+axht)/H, axwt/W, axht/H])
+    # Bottom left
+    ax10 = fig1.add_axes([pad/W, pad/H, axwt/W, axht/H])
+    # Bottom right
+    ax11 = fig1.add_axes([(pad+hgap+axwt)/W, pad/H, axwt/W, axht/H])
+    
+    return fig1, ax00, ax01, ax10, ax11
+
+def make21():
+    '''
+    Make a 2x1 subplot, properly layed out.
+    Returns: fig, axtop, axbot
+    Good with 'mysty.mplstyle'
+    '''
+    hpad = 1.5
+    vpad = 1
+    vgap = 1.5
+    axht = 4
+    axwt = 5
+    W = 2*hpad+axwt
+    H = 2*vpad+2*axht+vgap
+    fig = p.figure(figsize=(W,H))
+    axtop = fig.add_axes([hpad/W,(vpad+axht+vgap)/H,axwt/W,axht/H])
+    axbot = fig.add_axes([hpad/W,vpad/H,axwt/W,axht/H])
+    
+    return fig, axtop, axbot
+    
+def make12():
+    '''
+    Make a 1x2 subplot, properly layed out.
+    Returns: fig, axleft, axrt
+    Good with 'mysty.mplstyle'
+    '''
+    hpad = 1.5
+    vpad = 1
+    hgap = 2
+    axht = 4
+    axwt = 5
+    W = 2*hpad+2*axwt+hgap
+    H = 2*vpad+axht
+    fig = p.figure(figsize=(W,H))
+    axleft = fig.add_axes([hpad/W,vpad/H,axwt/W,axht/H])
+    axrt = fig.add_axes([(hpad+axwt+hgap)/W,vpad/H,axwt/W,axht/H])
+    
+    return fig, axleft, axrt    
+    
+def ksi2Mpa(ksi):
+    """
+    Multipls ksi by 6.89475908677537
+    """
+    return ksi * 6.89475908677537
+
+def TickLabelFS(ax,fs=30,which='both'):
+    '''
+    Pass a figure handle.
+    fs=fontsize
+    Which specifies which axis (x or y) you want to change the ticklabel size of
+    which = 'both' or two, 'x' or 0, 'y' or 1
+    '''
+    xls, yls = ax.xaxis.get_ticklabels(), ax.yaxis.get_ticklabels()
+    if which in ['both' , 2]:
+        itlist = [xls,yls]
+    elif which in ['x',0]:
+        itlist = [xls]
+    elif which in ['y',1]:
+        itlist = [yls]
+    for labels in itlist:
+        for i in labels:
+            i.set_fontsize(fs)
+    p.draw()
+    return None
+
+def data2axes(ax,coords):
+    '''
+    Converts data coords into axes coords.
+    Most handy for adding annotation MyArrows within the axes box via ginput.
+    '''
+    return ax.transLimits.transform(coords)
+    
+def eztext(ax,text,loc='upper left',**kwargs):
+    '''
+    For placing text in the corners of the axis.
+    Give the axis handle, the text, and the location.
+    Location parameters:
+        'upper left' = 'ul' = 0 = 'top left' = 'tl'
+        'upper right' = 'ur' = 1 = 'top right' = 'tr'
+        'lower right = 'lr'  = 2 = 'bottom right'  = 'br'
+        'lower left' = 'll' = 3 = 'bottom left' = 'bl'
+    '''
+    if loc in ['upper left', 'ul', 0, 'top left', 'tl']:
+        x, y = .01, .98
+        ha, va = 'left', 'top'
+    elif loc in ['upper right', 'ur', 1, 'top right', 'tr']:
+        x, y = .99, .98
+        ha, va = 'right', 'top'
+    elif loc in ['lower right', 'lr', 2, 'bottom right', 'br']:
+        x, y = .99, .01
+        ha, va = 'right', 'bottom'
+    elif loc in ['lower left', 'll', 3, 'bottom left', 'bl']:
+        x, y = .01, .01
+        ha, va = 'left', 'bottom'
+    else:
+        raise ValueError('Got unknown location, "{}"'.format(loc))
+    tx = ax.text(x,y,text,ha=ha,va=va,transform=ax.transAxes,**kwargs)
+    p.draw()
+    return tx
+
+def colorbar(ax,cbar,AL=.2,HL=.045,HW=.5,OH=.3,TW=.0035,PLW=0):
+    '''
+    colorbar(ax,cbar,AL=.2,HL=.045,HW=.5,OH=.3,TW=.0035,PLW=0)
+    
+    *** Something seriously weird about how this (doesn't) works.
+        When a script calling this function is just run, it renders 
+        the arrow way far to the right of the cbar.
+        When plotting interactively in ipython and a long code block is pasted into the QTconsole
+        and f.colorbar() is contained in the code block, the arrow renders way far to the right.
+        But if plotting interactively and I do everything the same except call f.colorbar, 
+        then call f.colorbar on its own after the figure window has appeared, it works fine.
+    ***
+    
+    Formats the colorbar label appropriately and adds an arrow that is identical to the 
+    arrow on the figure axis.
+    
+    ax : the axis of the figure plot, NOT THE COLORBAR AXIS
+    cbar : the colorbar, NOT THE COLORBAR AXIS
+    The rest of the arguments are arrow props.
+    Some day maybe I'll have it copy the y-axis arrow rather than specifying props.
+    '''
+    fig = ax.get_figure()
+    cax = cbar.ax
+    #An inverse transform that will take me from display to data coordinates
+    # Notice that it is the plot axes, NOT the colorbar axes
+    inv = ax.transAxes.inverted()
+    #Handle for renderer
+    R = fig.canvas.get_renderer()
+    caxY = cax.yaxis
+    ylab = caxY.get_label()
+    
+    if ylab.get_text().find(r'\frac') != -1:
+            ylab.set_size(30)
+    
+    ticklabelbbox = inv.transform( caxY.get_ticklabel_extents(R)[1].get_points() )
+    ylab.set_verticalalignment('center')
+    ylab.set_horizontalalignment('center')
+    ylab.set_rotation(0)
+    xpos, ypos = n.max(ticklabelbbox[:,0]), (3/4)
+    caxY.set_label_coords( xpos , ypos , transform = ax.transAxes )
+    bboxLy = inv.transform(ylab.get_window_extent(R).get_points())
+    xpos = xpos + ( n.max(bboxLy[:,0]) - xpos )
+    caxY.set_label_coords( xpos , ypos , transform = ax.transAxes)
+    # Set arrow parameters and draw the arrow
+    bot = n.min(bboxLy[:,1])
+    OH1=OH
+    HL1=HL
+    HW1=HW
+    TW1=TW
+    AL1=AL
+    ar=MyArrow(xpos,bot-AL1-HL1/2,0,AL1,head_length=HL1,overhang=OH1, head_width=HW1,
+                    tail_width=TW1,lw=PLW,transform=ax.transAxes,color='k')
+    ax.add_patch(ar)
+    ar.set_clip_on(False)
+    p.draw()
+    return ar
 
 class MyArrow(Polygon):
     """
@@ -313,204 +481,4 @@ class MyArrow(Polygon):
             #p.figure(7)
             #p.plot(verts[:,0],verts[:,1])
             verts = n.dot(coords, M) + [x+dx, y+dy]
-        Polygon.__init__(self, list(map(tuple, verts)), closed=True, **kwargs)
-
-def ksi2Mpa(ksi):
-    """
-    Multipls ksi by 6.89475908677537
-    """
-    return ksi * 6.89475908677537
-
-def TickLabelFS(ax,fs=30,which='both'):
-    '''
-    Pass a figure handle.
-    fs=fontsize
-    Which specifies which axis (x or y) you want to change the ticklabel size of
-    which = 'both' or two, 'x' or 0, 'y' or 1
-    '''
-    xls, yls = ax.xaxis.get_ticklabels(), ax.yaxis.get_ticklabels()
-    if which in ['both' , 2]:
-        itlist = [xls,yls]
-    elif which in ['x',0]:
-        itlist = [xls]
-    elif which in ['y',1]:
-        itlist = [yls]
-    for labels in itlist:
-        for i in labels:
-            i.set_fontsize(fs)
-    p.draw()
-    return None
-
-def data2axes(ax,coords):
-    '''
-    Converts data coords into axes coords.
-    Most handy for adding annotation MyArrows within the axes box via ginput.
-    '''
-    return ax.transLimits.transform(coords)
-    
-def eztext(ax,text,loc='upper left',**kwargs):
-    '''
-    For placing text in the corners of the axis.
-    Give the axis handle, the text, and the location.
-    Location parameters:
-        'upper left' = 'ul' = 0 = 'top left' = 'tl'
-        'upper right' = 'ur' = 1 = 'top right' = 'tr'
-        'lower right = 'lr'  = 2 = 'bottom right'  = 'br'
-        'lower left' = 'll' = 3 = 'bottom left' = 'bl'
-    '''
-    if loc in ['upper left', 'ul', 0, 'top left', 'tl']:
-        x, y = .01, .98
-        ha, va = 'left', 'top'
-    elif loc in ['upper right', 'ur', 1, 'top right', 'tr']:
-        x, y = .99, .98
-        ha, va = 'right', 'top'
-    elif loc in ['lower right', 'lr', 2, 'bottom right', 'br']:
-        x, y = .99, .01
-        ha, va = 'right', 'bottom'
-    elif loc in ['lower left', 'll', 3, 'bottom left', 'bl']:
-        x, y = .01, .01
-        ha, va = 'left', 'bottom'
-    else:
-        raise ValueError('Got unknown location, "{}"'.format(loc))
-    tx = ax.text(x,y,text,ha=ha,va=va,transform=ax.transAxes,**kwargs)
-    p.draw()
-    return tx
-
-class FancyArrow_original(Polygon):
-    """
-    Like Arrow, but lets you set head width and head height independently.
-    """
-
-    def __str__(self):
-        return "FancyArrow()"
-
-    def __init__(self, x, y, dx, dy, width=0.001, length_includes_head=False,
-        head_width=None, head_length=None, shape='full', overhang=0,
-        head_starts_at_zero=False, **kwargs):
-        """
-        Constructor arguments
-          *width*: float (default: 0.001)
-            width of full arrow tail
-          *length_includes_head*: [True | False] (default: False)
-            True if head is to be counted in calculating the length.
-          *head_width*: float or None (default: 3*width)
-            total width of the full arrow head
-          *head_length*: float or None (default: 1.5 * head_width)
-            length of arrow head
-          *shape*: ['full', 'left', 'right'] (default: 'full')
-            draw the left-half, right-half, or full arrow
-          *overhang*: float (default: 0)
-            fraction that the arrow is swept back (0 overhang means
-            triangular shape). Can be negative or greater than one.
-          *head_starts_at_zero*: [True | False] (default: False)
-            if True, the head starts being drawn at coordinate 0
-            instead of ending at coordinate 0.
-        Other valid kwargs (inherited from :class:`Patch`) are:
-        %(Patch)s
-        """
-        if head_width is None:
-            head_width = 20 * width
-        if head_length is None:
-            head_length = 1.5 * head_width
-
-        distance = np.sqrt(dx ** 2 + dy ** 2)
-        if length_includes_head:
-            length = distance
-        else:
-            length = distance + head_length
-        if not length:
-            verts = []  # display nothing if empty
-        else:
-            # start by drawing horizontal arrow, point at (0,0)
-            hw, hl, hs, lw = head_width, head_length, overhang, width
-            left_half_arrow = np.array([
-               [0.0, 0.0],                  # tip
-                [-hl, -hw / 2.0],             # leftmost
-                [-hl * (1 - hs), -lw / 2.0],  # meets stem
-                [-length, -lw / 2.0],          # bottom left
-                [-length, 0],
-            ])
-            #if we're not including the head, shift up by head length
-            if not length_includes_head:
-                left_half_arrow += [head_length, 0]
-            #if the head starts at 0, shift up by another head length
-            if head_starts_at_zero:
-                left_half_arrow += [head_length / 2.0, 0]
-            #figure out the shape, and complete accordingly
-            if shape == 'left':
-                coords = left_half_arrow
-            else:
-                right_half_arrow = left_half_arrow * [1, -1]
-                if shape == 'right':
-                    coords = right_half_arrow
-                elif shape == 'full':
-                    # The half-arrows contain the midpoint of the stem,
-                    # which we can omit from the full arrow. Including it
-                    # twice caused a problem with xpdf.
-                    coords = np.concatenate([left_half_arrow[:-1],
-                                             right_half_arrow[-2::-1]])
-                else:
-                    raise ValueError("Got unknown shape: %s" % shape)
-            cx = float(dx) / distance
-            sx = float(dy) / distance
-            M = np.array([[cx, sx], [-sx, cx]])
-            verts = np.dot(coords, M) + (x + dx, y + dy)
-
-        Polygon.__init__(self, list(map(tuple, verts)), closed=True, **kwargs)
-
-def colorbar(ax,cbar,AL=.2,HL=.045,HW=.5,OH=.3,TW=.0035,PLW=0):
-    '''
-    colorbar(ax,cbar,AL=.2,HL=.045,HW=.5,OH=.3,TW=.0035,PLW=0)
-    
-    *** Something seriously weird about how this (doesn't) works.
-        When a script calling this function is just run, it renders 
-        the arrow way far to the right of the cbar.
-        When plotting interactively in ipython and a long code block is pasted into the QTconsole
-        and f.colorbar() is contained in the code block, the arrow renders way far to the right.
-        But if plotting interactively and I do everything the same except call f.colorbar, 
-        then call f.colorbar on its own after the figure window has appeared, it works fine.
-    ***
-    
-    Formats the colorbar label appropriately and adds an arrow that is identical to the 
-    arrow on the figure axis.
-    
-    ax : the axis of the figure plot, NOT THE COLORBAR AXIS
-    cbar : the colorbar, NOT THE COLORBAR AXIS
-    The rest of the arguments are arrow props.
-    Some day maybe I'll have it copy the y-axis arrow rather than specifying props.
-    '''
-    fig = ax.get_figure()
-    cax = cbar.ax
-    #An inverse transform that will take me from display to data coordinates
-    # Notice that it is the plot axes, NOT the colorbar axes
-    inv = ax.transAxes.inverted()
-    #Handle for renderer
-    R = fig.canvas.get_renderer()
-    caxY = cax.yaxis
-    ylab = caxY.get_label()
-    
-    if ylab.get_text().find(r'\frac') != -1:
-            ylab.set_size(30)
-    
-    ticklabelbbox = inv.transform( caxY.get_ticklabel_extents(R)[1].get_points() )
-    ylab.set_verticalalignment('center')
-    ylab.set_horizontalalignment('center')
-    ylab.set_rotation(0)
-    xpos, ypos = n.max(ticklabelbbox[:,0]), (3/4)
-    caxY.set_label_coords( xpos , ypos , transform = ax.transAxes )
-    bboxLy = inv.transform(ylab.get_window_extent(R).get_points())
-    xpos = xpos + ( n.max(bboxLy[:,0]) - xpos )
-    caxY.set_label_coords( xpos , ypos , transform = ax.transAxes)
-    # Set arrow parameters and draw the arrow
-    bot = n.min(bboxLy[:,1])
-    OH1=OH
-    HL1=HL
-    HW1=HW
-    TW1=TW
-    AL1=AL
-    ar=MyArrow(xpos,bot-AL1-HL1/2,0,AL1,head_length=HL1,overhang=OH1, head_width=HW1,
-                    tail_width=TW1,lw=PLW,transform=ax.transAxes,color='k')
-    ax.add_patch(ar)
-    ar.set_clip_on(False)
-    p.draw()
-    return ar
+        Polygon.__init__(self, list(map(tuple, verts)), closed=True, **kwargs)    
