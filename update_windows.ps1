6bac2388a73ef9802e55d4a465592497a73735b1
Russle <mfscales@gmail.com>
2017-05-09 17:30:45 -0500

    update_windows.ps1 created. Assuming the standard windows mplystyle dir and anaconda directory, run this to update them all.

---
+++ update_windows.ps1
@@ -0,0 +1,27 @@
+$url="https://raw.githubusercontent.com/saintsfan342000/MPL-a-al-SKK/master/mysty.mplstyle"
+$path="C:\Anaconda3\Lib\site-packages\matplotlib\mpl-data\stylelib/mysty.mplstyle"
+$client = New-Object System.Net.WebClient
+$client.DownloadFile($url, $path)
+
+$url="https://raw.githubusercontent.com/saintsfan342000/MPL-a-al-SKK/master/mysty-12.mplstyle"
+$path="C:\Anaconda3\Lib\site-packages\matplotlib\mpl-data\stylelib/mysty-12.mplstyle"
+$client = New-Object System.Net.WebClient
+$client.DownloadFile($url, $path)
+
+
+$url="https://raw.githubusercontent.com/saintsfan342000/MPL-a-al-SKK/master/mysty-sub.mplstyle"
+$path="C:\Anaconda3\Lib\site-packages\matplotlib\mpl-data\stylelib/mysty-sub.mplstyle"
+$client = New-Object System.Net.WebClient
+$client.DownloadFile($url, $path)
+
+
+$url="https://raw.githubusercontent.com/saintsfan342000/MPL-a-al-SKK/master/mysty-quad.mplstyle"
+$path="C:\Anaconda3\Lib\site-packages\matplotlib\mpl-data\stylelib/mysty-quad.mplstyle"
+$client = New-Object System.Net.WebClient
+$client.DownloadFile($url, $path)
+
+
+$url="https://raw.githubusercontent.com/saintsfan342000/MPL-a-al-SKK/master/figfun.py"
+$path="C:\Anaconda3\figfun.py"
+$client = New-Object System.Net.WebClient
+$client.DownloadFile($url, $path)
